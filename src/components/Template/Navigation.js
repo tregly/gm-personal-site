8938584a8d1678e8a6dcbe77482411309660edf5
Giuseppe Maldarelli <giuseppe.maldarelli@outlook.it>
2021-11-10 12:04:37 +0100

    Stats page e production env

--- src/components/Template/Navigation.js
+++ src/components/Template/Navigation.js
@@ -7,15 +7,15 @@ import routes from '../../data/routes';
 // Websites Navbar, displays routes defined in 'src/data/routes'
 const Navigation = () => (
   <header id="header">
-    <h1 className="index-link">
-      {routes.filter((l) => l.index).map((l) => (
-        <Link key={l.label} to={l.path}>{l.label}</Link>
-      ))}
-    </h1>
-    <nav className="links">
-      <ul>
+    <nav className="links navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          {routes.filter((l) => l.index).map((l) => (
+            <Link key={l.label} to={l.path}>{l.label}</Link>
+          ))}
+        </li>
         {routes.filter((l) => !l.index).map((l) => (
-          <li key={l.label}>
+          <li key={l.label} className="nav-item">
             <Link to={l.path}>{l.label}</Link>
           </li>
         ))}
